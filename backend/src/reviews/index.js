f651651b9408b7b6542ff112979fe35a1d794193
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-03 10:39:20 +0000

    Added Review Schema

--- backend/src/reviews/index.js
+++ backend/src/reviews/index.js
@@ -12,7 +12,6 @@ import { parseFile, uploadFile } from "../utils/upload/index.js";
 import {
   checkReviewSchema
 } from "./validation.js";
-import { check } from "express-validator";
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -59,7 +58,62 @@ reviewsRouter.get("/", async (req, res, next) => {
     } catch (error) {
         res.status(500).send({ message: error.message });
     }
- })
+});
+
+// Edit Review
+
+reviewsRouter.get("/", async (req, res, next) => {
+    try {
+        const fileAsBuffer = fs.readFileSync(reviewsFilePath);
+        
+        const fileAsString = fileAsBuffer.toString();
+
+        const fileAsJSONArray = JSON.parse(fileAsString);
+
+        const reviewIndex = fileAsJSONArray.findIndex((review) => review.id === req.params.id);
+        if (!reviewIndex == -1) {
+            res.status(404).send({ message: error.message });
+        }
+        const previousReviewData = fileAsJSONArray[blogIndex];
+        const changedReview = {
+            ...previousReviewData,
+            ...req.body,
+            updatedAt: newDate(),
+            id: req.params.id,
+        };
+        fileAsJSONArray[reviewIndex] = changedReview;
+
+        fs.writeFileSync(reviewsFilePath, JSON.stringify(fileAsJSONArray));
+
+        res.send(changedReview);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+// Delete Review
+
+reviewsRouter.delete("/:id", async (req, res, next) => {
+    try {
+        const fileAsBuffer = fs.readFileSync(reviewsFilePath);
+
+        const fileAsString = fileAsBuffer.toString();
+
+        const fileAsJSONArray = JSON.parse(fileAsString);
+
+        const review = fileAsJSONArray.find((review) => review.id === req.params.id);
+        if (!review) {
+            res.status(404).send({ message: `Review with ${req.params.id} does not exist.` });
+        }
+        fileAsJSONArray = fileAsJSONArray.filter((review) => review.id);
+
+        fs.writeFileSync(reviewsFilePath, JSON.stringify(fileAsJSONArray));
+
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
 
 
 
--- backend/src/reviews/validation.js
+++ backend/src/reviews/validation.js
@@ -1,2 +1,22 @@
 import { checkSchema, validationResult } from "express-validator";
 
+const schema = {
+    comment: {
+        in: ["body"],
+        isString: {
+            errorMessage: "Content validation failed, type must be string.",
+        },
+    },
+    rate: {
+        in: ["body"],
+        isNumeric: {
+            errorMessage: "Rating validation failed, type must be string.",
+        },
+    },
+    productId: {
+        in: ["body"],
+        isString: {
+            errorMessage: "Product ID validation failed, type must be string.",
+        },
+    },
+};
