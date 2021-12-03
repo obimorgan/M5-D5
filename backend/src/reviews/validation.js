56ea2c2bf83721220c77f033e7018cf549140ea9
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-03 10:43:36 +0000

    Updated Review Validation

--- backend/src/reviews/validation.js
+++ backend/src/reviews/validation.js
@@ -20,3 +20,16 @@ const schema = {
         },
     },
 };
+
+export const checkReviewSchema = checkSchema(schema);
+
+export const checkValidationResult = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error("Review post validation has failed.");
+        error.status = 400;
+        error.errors = errors.array();
+        next(error);
+    }
+    next();
+};
